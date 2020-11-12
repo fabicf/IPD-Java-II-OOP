9be000d47551131e012d7d2d560b2b70e49e832b
fabicf <fabiolacezarfaria@gmail.com>
2020-11-12 16:41:31 -0500

    Day00 Test

---
+++ Day00Test1/src/day00test1/Day00Test1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day00test1;
+
+/**
+ *
+ * @author 15144
+ */
+public class Day00Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        User user = new User();
+        user.firstName = "Fabi";
+        user.lastName = "Far";
+        
+        //System.out.println(user.firstName);
+        
+        user.output();
+    }
+
+    
+        
+}
---
+++ Day00Test1/src/day00test1/User.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day00test1;
+
+/**
+ *
+ * @author 15144
+ */
+class User {
+    public String firstName;
+    public String lastName;
+    
+    public void output(){
+        System.out.println(firstName + " " + lastName);
+    }
+}
